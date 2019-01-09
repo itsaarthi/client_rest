105f5f50f7a69b65a2fb07d1e013cbbd0b75ca45
itsaarthi <m.aarthi.laksmi@gmail.com>
2019-01-09 21:31:56 +0530

    update

--- db.js
+++ db.js
@@ -1,1 +1,9 @@
-console.log("Need to be done");
+
+var MongoClient = require('mongodb').MongoClient;
+
+
+MongoClient.connect("mongodb://<dbuser>:<dbpassword>@ds133041.mlab.com:33041/secure_auth", function(err, db) {
+  if(!err) {
+    console.log("Database connected");
+  }
+});
